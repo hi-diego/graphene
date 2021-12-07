8340c8bd7700ad06f288bfba95e4b9ef87904842
Diego Meza <hi@diego.pro>
2021-12-07 09:41:41 -0600

    Model renaming

---
+++ GrapheneCore/Models/IModelLog.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrapheneCore.Models
+{
+    public interface IModelLog
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        int ModelId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        //int? UserId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        string Model { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        string To { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        string From { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        string Event { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [NotMapped]
+        //[JsonIgnore]
+        EntityState InstanceEntityState { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [NotMapped]
+        Model Instance { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        Guid ModelUid { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entry"></param>
+        /// <returns></returns>
+        T InitializeGeneric<T>(EntityEntry entry, int? userId = null) where T : IModelLog, new();
+    }
+}
---
+++ GrapheneCore/Models/ModelLog.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrapheneCore.Models
+{
+    public class ModelLog: Model, IModelLog
+    {
+        public int ModelId { get; set; }
+        public int? UserId { get; set; }
+
+        //[ForeignKey(nameof(UserId))]
+        //public User User { get; set; }
+
+        public string Model { get; set; }
+        public string To { get; set; }
+        public string From { get; set; }
+        public string Event { get; set; }
+        public EntityState InstanceEntityState { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        [NotMapped]
+        public Model Instance { get; set; }
+        public Guid ModelUid { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entry"></param>
+        /// <returns></returns>
+        public virtual T InitializeGeneric<T>(EntityEntry entry, int? userId = null) where T : IModelLog, new()
+        {
+            T log = new T();
+            Model entity = ((Model)entry.Entity);
+            var to = entity.ToJson();
+            var copy = entry.OriginalValues.Clone().ToObject();
+            var from = ((Model)copy).ToJson();
+            log.InstanceEntityState = entry.State;
+            //log.UserId = userId;
+            log.Model = entry.Entity.GetType().Name;
+            log.ModelId = ((Model)entry.Entity).Id;
+            log.To = to;
+            log.From = from;
+            log.Event = entry.State.ToString();
+            log.CreatedAt = DateTime.Now;
+            log.Instance = ((Model)entry.Entity);
+            log.ModelUid = ((Model)entry.Entity).Uid;
+            return log;
+        }
+    }
+}
