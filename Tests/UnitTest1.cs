f08f9f6594f119291a973cf785f06dfa59d1fbde
DiegoMeza <hi@diego.pro>
2021-12-19 09:02:17 -0600

    Test Actions with new naming

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -2,12 +2,18 @@ using Xunit;
 
 namespace Tests
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class UnitTest1
     {
+        /// <summary>
+        /// 
+        /// </summary>
         [Fact]
         public void Test1()
         {
-
+            //
         }
     }
 }
