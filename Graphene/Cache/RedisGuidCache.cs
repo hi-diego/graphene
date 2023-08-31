55fb9c30543cfdac2d5744bb478593c93277cad5
DiegoM <xenia@diego.pro>
2023-08-31 02:57:01 -0600

    Redis Patter regex refactor

---
+++ Graphene/Cache/RedisGuidCache.cs
@@ -0,0 +1,116 @@
+using Graphene.Entities;
+using StackExchange.Redis;
+using System.Text.RegularExpressions;
+
+namespace Graphene.Cache
+{
+    public class RedisGuidCache {
+        public IDatabase Redis { get; set; }
+        public RedisGuidCache(IConnectionMultiplexer multiplexer)
+        {
+            Redis = multiplexer.GetDatabase();
+        }
+
+        public int GetCachedId (Guid guid)
+        {
+            string key = guid.ToString();
+            string? stringId = Redis.StringGet(key).ToString().Split(":").Last();
+            int id = stringId == null
+                ? 0
+                : Int32.Parse(stringId);
+            return id;
+        }
+
+        public Guid GetCachedGuid (int id, string entityName)
+        {
+            string key = GetIdKey(id, entityName);
+            string? guidString = Redis.StringGet(key).ToString();
+            Guid guid = guidString == null
+                ? Guid.Empty
+                : new Guid(guidString);
+            return guid;
+        }
+
+        public string GetIdKey (int id, string entityName)
+        {
+            return entityName + ":" +  id.ToString();
+        }
+
+        public string GetIdKey (Entity instance)
+        {
+            return GetIdKey(instance.Id, instance._Entity);
+        }
+
+        public void CacheIds (Entity instance)
+        {
+            string uid = (string)instance.Uid.ToString();
+            string id = (string)instance.Id.ToString();
+            Redis.StringSetAsync(uid, GetIdKey(instance));
+            Redis.StringSetAsync(GetIdKey(instance), uid);
+        }
+
+        public void CacheManyIds (IEnumerable<Entity> instances)
+        {
+            // instances
+            // string uid = (string)instance.Uid.ToString();
+            // string id = (string)instance.Id.ToString();
+            // Redis.StringSetAsync(GetIdKey(instance), uid);
+            // Redis.StringSetAsync(uid, GetIdKey(instance));
+        }
+
+        public Dictionary<string, string> GetGuidDictionary (IEnumerable<string> guids, bool full = true)
+        {
+            var uniqueGuids = guids.ToHashSet().ToArray();
+            var idStrings = GetIds(uniqueGuids, full).ToArray();
+            var i = 0;
+            return idStrings.ToDictionary(v => uniqueGuids[i++]);
+        }
+
+        public IEnumerable<string> GetIds (IEnumerable<string> guids, bool full = true)
+        {
+            var redisKeys = guids.ToHashSet().Select(uid => new RedisKey(uid)).ToArray();
+            var redisValues = Redis.StringGet(redisKeys);
+            var idStrings = redisValues.Select(id => full ? $"{id}" : $"{id}".Split(':').Last());
+            return idStrings;
+        }
+
+        public IEnumerable<string> GetIds (IEnumerable<Guid> guids)
+        {
+            return GetIds(guids.Select(g => g.ToString()));
+        }
+        public string ReplaceGuidsWithIds (string input)
+        {
+            string pattern = @"([\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12})";
+            // match all the guids
+            var matches = Regex.Matches(input, pattern, RegexOptions.IgnoreCase);
+            // filter and make them unique 
+            var guids = matches.Select(m => m.Value);
+            // Get dictionary: guids as keys, can posibly have null values, but not null keys
+            var guidDic = GetGuidDictionary(guids, false);
+            // replace it
+            var output = guidDic.Aggregate(input, (o, gi) => o.Replace(gi.Key, gi.Value));
+            return output;
+        }
+
+        public string ReplaceIdsWithGuids (string input)
+        {
+            string pattern = Pattern();
+            // match all the guids
+            var matches = Regex.Matches(input, pattern, RegexOptions.IgnoreCase);
+            // filter and make them unique 
+            var idsTemplate = matches.Select(m => m.Value.Split(Separator()).Last()).ToHashSet();
+            // Get dictionary: guids as keys, can posibly have null values, but not null keys
+            var ids = GetIds(idsTemplate).ToArray();
+            // init index to acces ids array
+            var i = 0;
+            // replace it
+            var output = idsTemplate.Aggregate(input, (o, replace) => o = o.Replace(ReplaceString(replace), ids[i++]));
+            return output;
+        }
+        public static string FormatId (string entityName, object? id) => ReplaceString($"{entityName}:{id}");
+        public static string Separator () => "ᴙ";
+        public static string ReplaceString (string value) => $"{Separator()}{value}";
+        public static string SubPattern() => @"\w*:\d*";
+        public static string Pattern() => ReplaceString($"({SubPattern()})");
+    }
+}
---
+++ Graphene/Http/Converters/RedisGuidReplaceConverter.cs
@@ -0,0 +1,64 @@
+﻿using Graphene.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Graphene.Cache;
+
+namespace Graphene.Http.Converters
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="E"></typeparam>
+    public class RedisGuidReplaceConverter<E> : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return true;
+        }
+
+        public override object? ReadJson(JsonReader reader, Type objectType, object? v, JsonSerializer serializer)
+        {
+            int id = 0;
+            Int32.TryParse((string) reader.Value, out id);
+            return id;
+        }
+
+        public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+        {
+            writer.WriteValue(RedisGuidCache.FormatId(typeof(E).Name, value));
+        }
+    }
+
+    class ConfigureJsonOptions : IConfigureOptions<MvcNewtonsoftJsonOptions>
+    {
+        //private readonly IGrapheneDatabaseContext _db;
+        private readonly Microsoft.AspNetCore.Http.IHttpContextAccessor _httpContextAccessor;
+        private readonly IServiceProvider _serviceProvider;
+
+        public ConfigureJsonOptions(
+            Microsoft.AspNetCore.Http.IHttpContextAccessor httpContextAccessor,
+            //IGrapheneDatabaseContext db,
+            IServiceProvider serviceProvider)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            //_db = db;
+            _serviceProvider = serviceProvider;
+        }
+
+        public void Configure(MvcNewtonsoftJsonOptions options)
+        {
+            var sp = new ServiceProviderConverter(_httpContextAccessor, _serviceProvider);
+            JsonConvert.DefaultSettings = () => {
+                var settings = new JsonSerializerSettings();
+                settings.Converters.Add(sp);
+                settings.DateFormatString = "yyyy-MM-ddTHH:mm:ss.fffffffK";
+                settings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                return settings;
+            };
+            options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            options.SerializerSettings.DateFormatString = "yyyy-MM-ddTHH:mm:ss.fffffffK";
+            options.SerializerSettings.Converters.Add(sp);
+        }
+    }
+}
